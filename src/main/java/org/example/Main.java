513bced0b91e85c51d06befbd9407a5613d164e4
Fredrik Welander <welandfr@arcada.fi>
2024-01-31 10:05:08 +0200

    Testing nextInt with one arg

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Java version: " + System.getProperty("java.version"));
 
-        System.out.println("random: " + (new Random()).nextInt(10,20));
+        //System.out.println((new Random()).nextInt(10,20));
+
+        System.out.println((new Random()).nextInt(10) + 10);
 
     }
 }
