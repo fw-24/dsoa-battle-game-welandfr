a91769cca65424fd40d79f25758cf873981823f0
Fredrik Welander <welandfr@arcada.fi>
2024-01-31 10:12:33 +0200

    nextInt commented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Java version: " + System.getProperty("java.version"));
 
-        //System.out.println((new Random()).nextInt(10,20));
+        // Funkar INTE i GitHub Classroom:
+        // System.out.println((new Random()).nextInt(10,20));
 
+        // FUNKAR  i GitHub Classroom:
         System.out.println((new Random()).nextInt(10) + 10);
 
     }
